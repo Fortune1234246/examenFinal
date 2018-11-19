cd41c82f81f3f8424a6ecb930a1da6e06d462429
Fortune1234246 <fortune1234246@gmail.com>
2018-11-19 11:08:29 -0600

    Template base y listar materias

--- examen/admin.py
+++ examen/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Materia)
-admin.site.register(Grado)
+admin.site.register(Materia, MateriaAdmin)
+admin.site.register(Grado, GradoAdmin)
 admin.site.register(asignacion)
--- examen/views.py
+++ examen/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from examen.models import Materia
 
 # Create your views here.
+
+
+def listar_materia(request):
+    materia = Materia.objects.all()
+    return render(request, 'examen/listarmaterias.html', {'materia': materia})
